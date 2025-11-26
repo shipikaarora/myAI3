6f36abe4eddbedcbd72cec605bcc383cb471dd11
shipikaarora <shipikaarora98@gmail.com>
2025-11-26 19:15:51 +0530

    Update user-message.tsx

--- components/messages/user-message.tsx
+++ components/messages/user-message.tsx
@@ -1,19 +1,9 @@
-import { UIMessage } from "ai";
-import { Response } from "@/components/ai-elements/response";
+import { Message } from "ai";
 
-export function UserMessage({ message }: { message: UIMessage }) {
-    return (
-        <div className="whitespace-pre-wrap w-full flex justify-end">
-            <div className="max-w-lg w-fit px-4 py-3 rounded-[20px] bg-neutral-100">
-                <div className="text-sm">
-                    {message.parts.map((part, i) => {
-                        switch (part.type) {
-                            case "text":
-                                return <Response key={`${message.id}-${i}`}>{part.text}</Response>;
-                        }
-                    })}
-                </div>
-            </div>
-        </div>
-    )
+export function UserMessage({ message }: { message: Message }) {
+  return (
+    <div className="bubble-user">
+      {message.content}
+    </div>
+  );
 }
