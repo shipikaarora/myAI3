19f1f813d1ca9bba2d2f9ad94fe1bff959ec944e
shipikaarora <shipikaarora98@gmail.com>
2025-11-27 02:21:43 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -166,8 +166,8 @@ export default function Chat() {
     <Image
       src="/logo.png"
       alt="Logo"
-      width={80}
-      height={80}
+      width={60}
+      height={60}
       className="object-contain"
       priority
     />
